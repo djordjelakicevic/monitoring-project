468185fbf58239f52e0f88c71e1ac13035df0918
djordjelakicevic <projects@djordjelakicevic.com>
2025-06-09 18:17:43 +0200

    Updated incus launch script

--- scripts/incus_launch_auto.sh
+++ scripts/incus_launch_auto.sh
@@ -2,10 +2,14 @@
 ### Script for automating creating incus containers ###
 
 ### VARS ###
-IMAGE="debian/12"
-PROFILE="incusbr-profile"
-CONTAINER_NAME="djole-djoledjole"
-INTERFACE_NAME="incusbr0"
+#IMAGE="debian/12"
+#PROFILE="incusbr-profile"
+#CONTAINER_NAME="djole-djoledjole"
+#INTERFACE_NAME="incusbr0"
+CONTAINER_NAME="$1"
+IMAGE="$2"
+PROFILE="$3"
+INTERFACE_NAME="$4"
 NETWORK_FILE="/etc/systemd/network/'$INTERFACE_NAME'.network"
 
 ### Function for handling errors ###
@@ -13,6 +17,12 @@ handle_error() {
   echo -e "Error: $1"
   exit 1
 }
+
+if [ $# -lt 4 ]; then
+  echo -e "\n---> Usage: $0 <container_name> <image> <profile> <interface_name> <---"
+  echo -e "\n---> Example: $0 djole debian/12 eth0 <---"
+  exit 1
+fi
 
 incus launch images:"$IMAGE" "$CONTAINER_NAME" -p "$PROFILE" || handle_error "\n---> Container not created... Exiting... <---"
 sleep 10
@@ -27,8 +37,9 @@ DHCP=yes
 EONET
 networkctl reload
 sleep 10
-echo "\n---> Interface status: <---"
+echo -e "\n---> Interface status: <---"
 networkctl | grep "$INTERFACE_NAME"
 EOF
 
-echo -e "\n---> Container IP: $(incus ls | grep  "$CONTAINER_NAME"| awk '{ print $6 }') <--- "
+echo -e "\n---> Container IP: $(incus ls | grep  "$CONTAINER_NAME"| awk '{ print $6 }') <--- "#!/bin/bash
+### Script for automating creating incus containers ###
